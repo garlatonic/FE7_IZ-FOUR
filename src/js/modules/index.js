088263f55785691c2548fb578bf4320a73c59ef6
ivoryShield <sangapark34@naver.com>
2025-09-04 02:20:48 +0900

    올려

--- src/js/modules/index.js
+++
@@ -1,5 +0,0 @@
-import { getAllDocuments, viewDocument } from "../modules/api.js";
-import tooltip from "./tooltip";
-
-export default tooltip;
-
--- src/js/modules/modify.js
+++ src/js/modules/modify.js
@@ -1,7 +1,5 @@
 import { editDocument } from "./api.js";
-
-const ICON_FILE = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-icon lucide-file"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" /><path d="M14 2v4a2 2 0 0 0 2 2h4" /></svg>`;
-const ICON_FILEDATA = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-text-icon lucide-file-text"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M10 9H8"/><path d="M16 13H8"/><path d="M16 17H8"/></svg>`;
+import { ICON_FILE, ICON_FILEDATA } from "../components/icon.js";
 
 export function updatePage(id) {
   const pageTitle = document.querySelector(`[data-id="${id}"] .title`);
