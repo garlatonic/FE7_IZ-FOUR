b97b6ff0998a341172b155127a58dce6d643b584
ivoryShield <sangapark34@naver.com>
2025-09-03 16:22:36 +0900

    커밋시점 복구

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -1,33 +1,5 @@
-import {
-  getAllDocuments,
-  viewDocument,
-  postDocument,
-  editDocument,
-  deleteDocument,
-} from "./modules/document.js";
+import { getAllDocuments, viewDocument } from "../modules/api.js";
+import tooltip from "./tooltip";
 
-// 에디터 렌더 및 자동 저장 함수
-function openEditor(documentId, title = "", content = "") {
-  const titleInput = document.querySelector("#contents .title input");
-  const contentTextarea = document.querySelector("#contents .content textarea");
+export default tooltip;
 
-  if (!titleInput || !contentTextarea) return;
-
-  titleInput.value = title;
-  contentTextarea.value = content;
-
-  let saveTimeout;
-  function autoSave() {
-    clearTimeout(saveTimeout);
-    saveTimeout = setTimeout(() => {
-      editDocument(documentId, {
-        title: titleInput.value,
-        content: contentTextarea.value,
-      });
-    }, 1000);
-  }
-
-  titleInput.oninput = autoSave;
-  contentTextarea.oninput = autoSave;
-}
-window.openEditor = openEditor;
