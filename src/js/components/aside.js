9ece17ecb304a656fa63e1d992fcdc946e7080c8
Song YoungJu <moeirs545@gmail.com>
2025-09-03 15:21:10 +0900

    📝 브래드크럼 임시 완성 (트리 깊어지면 다시 봐야함)

--- src/js/components/aside.js
+++ src/js/components/aside.js
@@ -78,14 +78,51 @@ aside.init().then(() => {
   function navigator(state) {
     const pageArea = document.querySelector("#contents");
     const home = document.querySelector(".home");
+    const breadcrumb = document.querySelector(".breadcrumb ol");
+
+    // 브래드크럼 li 생성
+    const createBread = (text) => {
+      const breadLi = document.createElement("li");
+      const breadA = document.createElement("a");
+      breadA.textContent = text;
+      breadLi.appendChild(breadA);
+
+      return breadLi;
+    };
+
     if (state.isHome) {
       // 홈 화면
       home.classList.remove("hidden");
       pageArea.classList.add("hidden");
-      console.log("홈");
+      breadcrumb.replaceChildren(createBread("홈"));
     } else {
       pageArea.classList.remove("hidden");
       home.classList.add("hidden");
+
+      // 브래드크럼
+      const children = state.documents;
+      const depth = children.length;
+
+      if (depth === 0) {
+        // 자식페이지 또는 자식이 없는 부모페이지
+        const currentPage = document.querySelector(`[data-id="${state.id}"]`);
+        const parentPage = currentPage.parentElement.closest("li");
+
+        if (parentPage === null) {
+          // 자식 없는 부모 페이지
+          breadcrumb.replaceChildren(createBread(state.title));
+        } else {
+          const parent = parentPage.querySelector(".title").textContent;
+          breadcrumb.replaceChildren(
+            createBread("홈"),
+            createBread(parent),
+            createBread(state.title)
+          );
+        }
+      } else {
+        // 부모 페이지
+        breadcrumb.replaceChildren(createBread("홈"), createBread(state.title));
+      }
 
       // 페이지 이동할 때마다 바꿀 콘텐츠
       const div_inner = document.createElement("div");
@@ -160,7 +197,8 @@ aside.init().then(() => {
   window.addEventListener("popstate", (event) => {
     if (event.state !== null) {
       // 이전 페이지 기록 있으면
-      navigator(event.state);
+      const state = { event: event.type, ...event.state };
+      navigator(state);
     } else {
       // 없으면 같은 페이지에 머무르게
       history.replaceState(event.state, "", location.href);
@@ -199,16 +237,16 @@ document.addEventListener("DOMContentLoaded", () => {
         childList.appendChild(newLi);
 
         // 새 페이지 클릭 시 브래드크럼에 경로 추가
-        const newPageLink = newLi.querySelector(".page-item");
-        newPageLink.addEventListener("click", function (ev) {
-          ev.stopPropagation();
-          const breadcrumb = document.querySelector(".breadcrumb ol");
-          if (breadcrumb) {
-            const li = document.createElement("li");
-            li.innerHTML = `<a href="#none">새 페이지</a>`;
-            breadcrumb.appendChild(li);
-          }
-        });
+        // const newPageLink = newLi.querySelector(".page-item");
+        // newPageLink.addEventListener("click", function (ev) {
+        //   ev.stopPropagation();
+        //   const breadcrumb = document.querySelector(".breadcrumb ol");
+        //   if (breadcrumb) {
+        //     const li = document.createElement("li");
+        //     li.innerHTML = `<a href="#none">새 페이지</a>`;
+        //     breadcrumb.appendChild(li);
+        //   }
+        // });
       });
     });
 });
