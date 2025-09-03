f979bb0beaec3d01ed6987926c8e1b9e574ec87e
ivoryShield <sangapark34@naver.com>
2025-09-03 14:05:54 +0900

    🎨 Ul이 두번 감싸져서 수정

--- src/js/components/aside.js
+++ src/js/components/aside.js
@@ -19,7 +19,7 @@ const aside = {
   },
   buildItems(nodes, depth = 0) {
     console.log(nodes);
-    const temp = document.createElement("ul");
+    const ulEl = document.createElement("ul");
 
     for (const node of nodes) {
       const liEl = document.createElement("li");
@@ -54,17 +54,16 @@ const aside = {
       const children = node.documents || [];
 
       if (children.length > 0) {
-        const ulEl = document.createElement("ul");
-        ulEl.append(this.buildItems(children, depth + 1));
-        liEl.appendChild(ulEl);
+        const temp = document.createDocumentFragment();
+        temp.append(this.buildItems(children, depth + 1));
+        liEl.appendChild(temp);
       }
-      temp.append(liEl);
+      ulEl.append(liEl);
     }
 
-    return temp;
+    return ulEl;
   },
-  event() {
-  },
+  event() {},
 };
 
 aside.init();
@@ -84,64 +83,6 @@ aside.init();
 
 //   function navigator(state) {
 //     const pageArea = document.querySelector("#contents");
-//     if (state.isHome) {
-//       // 홈 화면
-//       const p = document.createElement("p");
-//       p.textContent = `제목: ${state.title} | 내용: ${state.content}`;
-//       pageArea.replaceChildren(p);
-//     } else {
-//       // const title = document.querySelector(".title h1 input");
-//       // title.value = state.title;
-//       // const content = document.querySelector(".content textarea");
-//       // content.value = state.content;
-
-//       // 페이지 이동할 때마다 바꿀 콘텐츠
-//       const div_inner = document.createElement("div");
-//       div_inner.classList.add("inner", "flex", "flex-col", "gap-4", "w-2/4");
-
-//       const div_title = document.createElement("div");
-//       div_title.className = "title";
-
-//       const h1 = document.createElement("h1");
-//       h1.classList.add("text-5xl", "font-bold");
-//       const h1_input = document.createElement("input");
-//       h1_input.type = "text";
-//       h1_input.placeholder = "새 페이지";
-//       h1_input.setAttribute(
-//         "aria-label",
-//         "해당 페이지의 제목을 편집하려면 입력하세요."
-//       );
-//       h1_input.classList.add("w-full", "outline-0");
-//       h1_input.value = state.title;
-
-//       h1.appendChild(h1_input);
-//       div_title.appendChild(h1);
-
-//       const div_content = document.createElement("div");
-//       div_content.classList.add("content", "flex-1");
-
-//       const textarea = document.createElement("textarea");
-//       textarea.rows = 1;
-//       textarea.placeholder = "내용을 입력하세요.";
-//       textarea.setAttribute(
-//         "aria-label",
-//         "해당 게시물의 내용을 편집하려면 입력하세요."
-//       );
-//       textarea.classList.add(
-//         "min-w-full",
-//         "min-h-full",
-//         "outline-0",
-//         "resize-none"
-//       );
-//       textarea.value = state.content;
-
-//       div_content.appendChild(textarea);
-
-//       div_inner.append(div_title, div_content);
-
-//       pageArea.replaceChildren(div_inner);
-//     }
-
 //     updatePage(state.id);
 //   }
 
